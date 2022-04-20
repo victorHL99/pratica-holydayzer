5ac97aaa2fe19cb07e88b0bf3db7da840c1a445d
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-20 15:13:40 -0300

    Requisitos terminados

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.listen(5500, () => console.log('server started'));
+app.use(cors());
+
+const holidays = [
+    { date: "1/1/2022", name: "Confraternização mundial" },
+    { date: "1/3/2022", name: "Carnaval" },
+    { date: "4/17/2022", name: "Páscoa" },
+    { date: "4/21/2022", name: "Tiradentes" },
+    { date: "5/1/2022", name: "Dia do trabalho" },
+    { date: "6/16/2022", name: "Corpus Christi" },
+    { date: "9/7/2022", name: "Independência do Brasil" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+    { date: "11/2/2022", name: "Finados" },
+    { date: "11/15/2022", name: "Proclamação da República" },
+    { date: "12/25/2022", name: "Natal" }
+];
+
+
+app.get("/holidays", (req, res) => {
+    res.send(holidays);
+});
+
+
+app.get ("/is-today-holiday", (req, res) => {
+
+    const hoje = new Date();
+    const novoHoje = hoje.toLocaleDateString();
+    const holiday = holidays.find(h => h.date === `${novoHoje}`);
+
+        if (holiday) {
+            res.send(`Sim, hoje é ${holiday.name}`);
+        } else {
+            res.send("Não, hoje não é feriado");
+        }
+
+})
